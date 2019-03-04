1b29558e0a309aa725608972edb51d481c3f9ec3
zx490336534 <490336534@qq.com>
2019-03-04 22:18:50 +0800

    创建workspace这个app并进行配置

--- Zxapitest/urls.py
+++ Zxapitest/urls.py
@@ -19,6 +19,7 @@ from login import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('',include('login.urls'))
+    path('',include('login.urls')),
+    path('',include('workspace.urls')),
 ]
 
---
+++ apps/workspace/urls.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#Author:zhongxin 
+#datetime:2019/3/4 10:16 PM 
