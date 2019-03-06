aa99048dbc856e21e139d6b89d0ac9f2cb35490d
zx490336534 <490336534@qq.com>
2019-03-06 20:59:33 +0800

    新建新增接口集的post函数及数据表

--- apps/workspace/models.py
+++ apps/workspace/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from utils.models import ModelBase
+
+
+class Callections(ModelBase):
+    name = models.CharField(max_length=64, verbose_name="接口集名称", help_text="接口集名称")
+
+    class Meta:
+        ordering = ['-update_time', '-id']
+        db_table = "tb_tag"  # 指明数据库表名
+
+    def __str__(self):
+        return self.name
--- apps/workspace/views.py
+++ apps/workspace/views.py
@@ -1,5 +1,11 @@
+import json
+
 from django.shortcuts import render
 from django.views import View
+from . import models
+from django.db.models import Count
+from utils.json_fun import to_json_data
+from utils.res_code import Code, error_map
 
 
 class IndexView(View):
@@ -9,4 +15,24 @@ class IndexView(View):
 
 class CallectionsView(View):
     def get(self, request):
-        return render(request, 'index/callections.html')
+        tags = models.Callections.objects.values('id', 'name')
+        return render(request, 'index/callections.html', locals())
+
+    def post(self, request):
+        json_data = request.body
+        if not json_data:
+            return to_json_data(errno=Code.PARAMERR, errmsg=error_map[Code.PARAMERR])
+        # 将json转化为dict
+        dict_data = json.loads(json_data.decode('utf8'))
+        tag_name = dict_data.get('name')
+        if tag_name and tag_name.strip():
+            tag_tuple = models.Callections.objects.get_or_create(name=tag_name)
+            tag_instance, tag_created_bolean = tag_tuple
+            new_tag_dict = {
+                "id": tag_instance.id,
+                "name": tag_instance.name
+            }
+            return to_json_data(errmsg="接口集创建成功", data=new_tag_dict) if tag_created_bolean else \
+                to_json_data(errno=Code.DATAEXIST, errmsg="接口集名已存在")
+        else:
+            return to_json_data(errno=Code.PARAMERR, errmsg="接口集名为空")
